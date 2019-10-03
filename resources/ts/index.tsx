cedf7a0af667833d16bf28fa5d4cec9cc6b93c18
jonas olsson <Jonas@jonass-MacBook-Pro.local>
2019-10-03 10:57:02 +0200

    Add react typings + super basic react app start

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -1,1 +1,6 @@
-console.log('Fire it up');
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+
